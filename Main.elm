05335340e64a0c8ba8a603c3c43b7bd2e072e021
Dave Compton <davecompton7@gmail.com>
2017-12-06 12:50:16 -0800

    added authorization request

--- Main.elm
+++ Main.elm
@@ -2,16 +2,19 @@ import Html exposing (Html, text, a, div)
 import Html.Attributes exposing (href)
 import Navigation exposing (..)
 import UrlParser as Url exposing (..)
+import Http exposing (..)
 
-type Oauth = Oauth (Maybe String) (Maybe String)
+type TokenData = TokenData (Maybe String) (Maybe String)
 
 type alias Model = 
-    { oauth: Maybe Oauth
+    { oauth: Maybe TokenData
+    , auth: Maybe String
     }
 
-type Msg = UrlChange Navigation.Location
+type Msg = UrlChange Navigation.Location | GetAuthorization (Result Error String)
 
 clientId = "8256469ec6a458a2b111"
+clientSecret = "b768bf69c0f44866330780a11d01cbf192ec0727"
 repoName = "oauthElm"
 redirectUri = "https://dc25.github.io/" ++ repoName
 scope = "repo:user"
@@ -24,21 +27,50 @@ githubOauthUri = "https://github.com/login/oauth/authorize"
                      ++ "&state=" ++ state
 
 
-oauthParser : Parser (Oauth -> a) a
-oauthParser = map Oauth (   s repoName 
+redirectParser : Parser (TokenData -> a) a
+redirectParser = map TokenData (   s repoName 
                         <?> stringParam "code" 
                         <?> stringParam "state"
                         )
 
+requestAuthorization : String -> Cmd Msg
+requestAuthorization code =
+    let url = "https://github.com/login/oauth/access_token"
+
+        headers = [(Http.header "Accept" "application/json")]
+
+        content =    "client_id=" ++ clientId 
+                  ++ "&client_secret=" ++ clientSecret 
+                  ++ "&code=" ++ code
+
+        -- mimetype per: https://stackoverflow.com/questions/46677608/how-to-specify-body-media-type-for-elm-post-request
+        body = stringBody "text/plain;charset=utf-8" content
+
+        rq = request 
+                 { method = "POST"
+                 , headers = headers
+                 , url = url
+                 , body = body
+                 , expect = expectString 
+                 , timeout = Nothing
+                 , withCredentials = False
+                 }
+    in send GetAuthorization rq
+
+
 init : Navigation.Location -> ( Model, Cmd Msg )
 init location =
-    let oauth = parsePath oauthParser location
+    let oauth = parsePath redirectParser location
     in case oauth of
-        Just _ -> ({oauth = oauth}, Cmd.none)
-        _ -> ({oauth = oauth}, Cmd.none)
+        Just (TokenData (Just code) (Just state)) -> ({oauth = oauth, auth = Nothing}, requestAuthorization code)
+        _ -> ({oauth = Nothing, auth = Nothing}, Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update _ m = (m, Cmd.none)
+update msg model = 
+    case msg of 
+        GetAuthorization (Err _) -> ({model | auth = Nothing} , Cmd.none)
+        GetAuthorization (Ok auth) -> ({model | auth = Just auth} , Cmd.none)
+        _ -> (model, Cmd.none)
 
 view : Model -> Html Msg
 view m = div []
