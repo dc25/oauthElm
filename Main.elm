f3170c98850a4620b8e62add3d1038ca75beef3e
Dave Compton <davecompton7@gmail.com>
2017-12-12 17:17:10 -0800

    updated

--- Main.elm
+++ Main.elm
@@ -131,7 +131,7 @@ update msg model =
 
 view : Model -> Html Msg
 view m = div []
-             [ a [href githubOauthUri] [text "authU"]
+             [ a [href githubOauthUri] [text "authV"]
              , text (toString m)
              ] 
 
