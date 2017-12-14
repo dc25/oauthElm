fdd95a965c9d47a1a37e7167f09730d42978c677
Dave Compton <davecompton7@gmail.com>
2017-12-13 16:13:36 -0800

    removing extra junk.

--- Main.elm
+++ Main.elm
@@ -11,17 +11,17 @@ type TokenData = TokenData (Maybe String) (Maybe String)
 type alias Model = 
     { oauth: Maybe TokenData
     , auth: Maybe (Result Error String)
-    , gazers: Maybe (List String)
     }
 
-type Msg = UrlChange Navigation.Location | GetAuthorization (Result Error String) | GazersFetched (Result Error (List String))
+type Msg =   UrlChange Navigation.Location 
+           | GetAuthorization (Result Error String) 
 
 clientId = "8256469ec6a458a2b111"
 clientSecret = "b768bf69c0f44866330780a11d01cbf192ec0727"
 repoName = "oauthElm"
 redirectUri = "https://dc25.github.io/" ++ repoName
 scope = "repo:user"
-state = "w9erwlksjdf;kajdsf"
+state = "w9erwlksjdf"
 
 githubOauthUri = "https://github.com/login/oauth/authorize"
                      ++ "?client_id=" ++ clientId 
@@ -38,71 +38,16 @@ redirectParser = Url.map TokenData
                      )
 
 
-
-
-decodeGazers = list (field "login" JD.string)
-
-
-getGazersCmd : String -> Cmd Msg
-getGazersCmd reponame = 
-   let
-       url = "https://api.github.com/repos/" ++ reponame ++ "/stargazers"
-
-
-       request = Http.get url decodeGazers
-  in
-    Http.send GazersFetched request
-
-requestAuthorizationHack : String -> Cmd Msg
-requestAuthorizationHack code =
-    let -- url = "https://github.com/login/oauth/access_token"
-        url = "http://172.17.0.2:8000/"
-        -- url = "http://127.0.0.1:8000/"
-        -- url = "https://api.github.com/repos/" ++ "dc25/solitaire" ++ "/stargazers"
-
-        -- headers = [(Http.header "Accept" "application/json")]
-        -- headers = [(Http.header "Content-Type" "application/x-www-form-urlencoded")]
-        headers = []
-
-        -- content =    "client_id=" ++ clientId 
-        --           ++ "&client_secret=" ++ clientSecret 
-        --           ++ "&code=" ++ code
-
-        -- mimetype per: https://stackoverflow.com/questions/46677608/how-to-specify-body-media-type-for-elm-post-request
-        -- body = stringBody "text/plain;charset=utf-8" content
-        body = emptyBody
-
-        rq = request 
-                 { method = "GET"
-                 , headers = headers
-                 , url = url
-                 , body = body
-                 , expect = expectJson decodeGazers
-                 , timeout = Nothing
-                 , withCredentials = False
-                 }
-    in send (GetAuthorization << Result.map String.concat) rq
-
 requestAuthorization : String -> Cmd Msg
 requestAuthorization code =
     let url = "https://github.com/login/oauth/access_token/"
-        -- url = "https://api.github.com/login/oauth/access_token/"
-        -- url = "http://127.0.0.1:8000/"
-        -- url = "http://172.17.0.2:8000/"
-
-        -- headers = [(Http.header "Accept" "application/json")]
-        headers = [ -- (Http.header "Content-Type" "application/x-www-form-urlencoded")
-                  (Http.header "Accept" "application/json")
-                  ]
+        headers = [ (Http.header "Accept" "application/json") ]
 
         content =    "client_id=" ++ clientId 
                   ++ "&client_secret=" ++ clientSecret 
                   ++ "&code=" ++ code
 
         body = stringBody "application/x-www-form-urlencoded" content
-        -- body = stringBody "" content
-
-        rqp = Http.post url  body JD.string
 
         rq = request 
                  { method = "POST"
@@ -113,25 +58,24 @@ requestAuthorization code =
                  , timeout = Nothing
                  , withCredentials = False
                  }
-    in send (GetAuthorization ) rq
+    in send GetAuthorization rq
 
 init : Navigation.Location -> ( Model, Cmd Msg )
 init location =
     let oauth = parsePath redirectParser location
     in case oauth of
-        Just (TokenData (Just code) (Just state)) -> ({oauth = oauth, auth = Nothing, gazers=Nothing}, requestAuthorization code)
-        _ -> ({oauth = Nothing, auth = Nothing, gazers=Nothing}, Cmd.none)
+        Just (TokenData (Just code) (Just _)) -> ({oauth = oauth, auth = Nothing}, requestAuthorization code)
+        _ -> ({oauth = Nothing, auth = Nothing}, Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
     case msg of 
         GetAuthorization (res) -> ({model | auth = Just res} , Cmd.none)
-        GazersFetched (Ok gazers) -> ({model | gazers = Just gazers} , Cmd.none)
         _ -> (model, Cmd.none)
 
 view : Model -> Html Msg
 view m = div []
-             [ a [href githubOauthUri] [text "buthe"]
+             [ a [href githubOauthUri] [text "Auth"]
              , text (toString m)
              ] 
 
